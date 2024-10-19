74b02d4ad81cb4e093b6ba7b6728850bcd3479aa
Solomon Eshun <Ssolomoneinsteinmit300@gmail.com>
2024-10-19 23:18:52 +0000

    ui-development: installs and setup ui library (React-Redux, NextUI)

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,58 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import path from "path";
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: { version: "18.3" },
+      "import-resolve": {
+        alias: {
+          map: [
+            ["@components", path.resolve(__dirname, "./src/components")],
+            ["@pages", path.resolve(__dirname, "./src/pages")],
+            ["@utils", path.resolve(__dirname, "./src/utils")],
+            ["@assets", path.resolve(__dirname, "./src/assets")],
+            ["@hooks", path.resolve(__dirname, "./src/hooks")],
+            ["@app", path.resolve(__dirname, "./src/app")],
+            ["@services", path.resolve(__dirname, "./src/services")],
+            ["@styles", path.resolve(__dirname, "./src/styles")],
+            ["@context", path.resolve(__dirname, "./src/context")],
+            ["@features", path.resolve(__dirname, "./src/features")],
+          ],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
   },
-]
+];
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path, { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@context': path.resolve(__dirname, './src/context'),
-    }
-  }
-})
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@features": path.resolve(__dirname, "./src/features"),
+    },
+  },
+});
